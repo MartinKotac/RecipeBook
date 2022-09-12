56761c0765f18f2b29e56688b14705390e376162
Martin Kotevski <Martin.Kotevski@netcetera.com>
2022-09-12 12:37:35 +0200

    Dynamic components creating component via code

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,19 +1,23 @@
-import {Component, ComponentFactoryResolver, OnInit} from '@angular/core';
+import {Component, ComponentFactoryResolver, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {NgForm} from "@angular/forms";
 import {AuthResponseData, AuthService} from "./auth.service";
-import {Observable} from "rxjs";
+import {Observable, Subscription} from "rxjs";
 import {Router} from "@angular/router";
 import {AlertComponent} from "../shared/alert/alert.component";
+import {PlaceholderDirective} from "../shared/placeholder/placeholder.directive";
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
-export class AuthComponent implements OnInit {
+export class AuthComponent implements OnInit, OnDestroy {
+
   isLogin = true;
   isLoading = false;
   error: string = '';
+  @ViewChild(PlaceholderDirective) alertHost!: PlaceholderDirective;
+  private closeSub!: Subscription;
 
   constructor(private authService: AuthService, private router: Router,
               private componentFactoryResolver: ComponentFactoryResolver) {
@@ -49,15 +53,28 @@ export class AuthComponent implements OnInit {
     });
     form.reset();
   }
-  onHandleError(){
+
+  onHandleError() {
     // @ts-ignore
     this.error = null;
   }
 
-  private showErrorAlert(message: string){
-   //const alertCmp = new AlertComponent();
+  private showErrorAlert(message: string) {
+    //const alertCmp = new AlertComponent();
     const alertCmpFactory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
+    const hostViewContainerRef = this.alertHost.viewContainerRef;
+    hostViewContainerRef.clear();
+    const componentRef = hostViewContainerRef.createComponent(alertCmpFactory);
+    componentRef.instance.message = message;
+    this.closeSub = componentRef.instance.close.subscribe(() => {
+      this.closeSub.unsubscribe();
+      hostViewContainerRef.clear();
+    })
+  }
 
+  ngOnDestroy(): void {
+    if (this.closeSub)
+      this.closeSub.unsubscribe();
   }
 
 }
--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -86,6 +86,5 @@ export class RecipeEditComponent implements OnInit {
 
   onCancel(){
     this.router.navigate(['../'],{relativeTo: this.activatedRoute})
-
   }
 }
