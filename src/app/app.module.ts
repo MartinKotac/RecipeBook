f7f50d440cc53567868e3406599c81369ea7b942
Martin Kotevski <Martin.Kotevski@netcetera.com>
2022-09-19 15:47:48 +0200

    Angular Modules: some examples, creating recipes module to reduce size of app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { ExerciseComponent } from './exercise/exercise.component';
-import { DropdownDirective } from './shared/dropdown.directive';
 import { ActiveUsersComponent } from './exercise/active-users/active-users.component';
 import { InactiveUsersComponent } from './exercise/inactive-users/inactive-users.component';
 import {RecipeService} from "./shared/services/recipe.service";
@@ -18,43 +11,32 @@ import {ShoppingListService} from "./shared/services/shopping-list.service";
 import {AppRoutingModule} from "./app-routing.module";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
-import { RecipesStartComponent } from './recipes/recipes-start/recipes-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { AuthComponent } from './auth/auth.component';
-import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
-import { AlertComponent } from './shared/alert/alert.component';
-import {PlaceholderDirective} from "./shared/placeholder/placeholder.directive";
+import {RecipesModule} from "./recipes/recipes.module";
+import {ShoppingListModule} from "./shopping-list/shopping-list.module";
+import {SharedModule} from "./shared/shared.module";
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
     ExerciseComponent,
-    DropdownDirective,
     ActiveUsersComponent,
     InactiveUsersComponent,
-    RecipesStartComponent,
-    RecipeEditComponent,
-    AuthComponent,
-    LoadingSpinnerComponent,
-    AlertComponent,
-    PlaceholderDirective
+    AuthComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RecipesModule,
+    ShoppingListModule,
+    SharedModule
   ],
-  providers: [RecipeService,ShoppingListService],
+  providers: [RecipeService, ShoppingListService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
